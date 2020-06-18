40f2348af704dbacdc2f27eb74094ef6aa19dfc2
unknown <Gvido@wwl-nb-20190430-i7.tes-rus.net>
2020-06-18 22:16:59 +0300

    Platformer

---
+++ Unity/Platformer/Assets/Script/TimedReload.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimedReload : MonoBehaviour
+{
+    [SerializeField] float time = 1;
+
+    public void Invoke()
+    {
+        Invoke("Reload", time);
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
